2604230ffc645f2c476985004fde7c16da295694
jeremystone <6453521+jeremystone@users.noreply.github.com>
2019-06-06 11:52:11 +0100

    Update template based on initial individual-losses-api

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 HM Revenue & Customs
+ * Copyright 2019 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,20 +22,39 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.11.0",
-    "uk.gov.hmrc" %% "domain" % "5.6.0-play-25",
-    "org.typelevel" %% "cats-core" % "1.1.0"
+    "uk.gov.hmrc"   %% "bootstrap-play-26" % "0.34.0",
+    "uk.gov.hmrc"   %% "domain"            % "5.6.0-play-26",
+    "uk.gov.hmrc"   %% "play-hmrc-api"     % "3.4.0-play-26",
+    "org.typelevel" %% "cats-core"         % "1.6.0"
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
-    "org.scalatest" %% "scalatest" % "3.0.4" % scope,
-    "org.scalamock" %% "scalamock" % "4.1.0" % scope,
-    "org.pegdown" % "pegdown" % "1.6.0" % scope,
-
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-
-    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % scope,
-
-    "com.github.tomakehurst" % "wiremock" % "2.6.0" % scope
+    "org.scalatest"          %% "scalatest"          % "3.0.5"             % scope,
+    "org.scalamock"          %% "scalamock"          % "4.1.0"             % scope,
+    "org.pegdown"            % "pegdown"             % "1.6.0"             % scope,
+    "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"             % scope,
+    "com.github.tomakehurst" % "wiremock"            % "2.22.0"            % scope
   )
+
+  // Fixes a transitive dependency clash between wiremock and scalatestplus-play
+  val overrides: Set[ModuleID] = {
+    val jettyFromWiremockVersion = "9.2.24.v20180105"
+    Set(
+      "org.eclipse.jetty"           % "jetty-client"       % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-continuation" % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-http"         % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-io"           % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-security"     % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-server"       % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-servlet"      % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-servlets"     % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-util"         % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-webapp"       % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-xml"          % jettyFromWiremockVersion,
+      "org.eclipse.jetty.websocket" % "websocket-api"      % jettyFromWiremockVersion,
+      "org.eclipse.jetty.websocket" % "websocket-client"   % jettyFromWiremockVersion,
+      "org.eclipse.jetty.websocket" % "websocket-common"   % jettyFromWiremockVersion
+    )
+  }
 }
