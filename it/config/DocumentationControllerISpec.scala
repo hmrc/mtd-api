1f820218db5b785259d3671eaa9e65fdba557ccc
Luke Jones <47660116+BetaDraconis@users.noreply.github.com>
2020-02-19 10:04:41 +0000

    add-test-coverage: Add test scenarios for config and definition files. (#24)
    * add-test-coverage: Add test scenarios for config and definition files.
    
    * add-test-coverage: Remove references to accounts API from spec.

--- it/config/DocumentationControllerISpec.scala
+++ it/config/DocumentationControllerISpec.scala
@@ -20,42 +20,44 @@ import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws.WSResponse
 import support.IntegrationBaseSpec
 
-class DocumentationISpec extends IntegrationBaseSpec {
+class DocumentationControllerISpec extends IntegrationBaseSpec {
 
   val apiDefinitionJson: JsValue = Json.parse(
     """
-      |{
-      |  "scopes":[
-      |    {
-      |      "key":"read:self-assessment",
-      |      "name":"#name#",
-      |      "description":"#desc#"
-      |    },
-      |    {
-      |      "key":"write:self-assessment",
-      |      "name":"#name#",
-      |      "description":"#desc#"
-      |    }
-      |  ],
-      |  "api":{
-      |    "name":"#mtd-api# (MTD)",
-      |    "description":"#desc#",
-      |    "context":"mtd/template",
-      |    "categories":["INCOME_TAX_MTD"],
-      |    "versions":[
-      |      {
-      |        "version":"1.0",
-      |        "status":"ALPHA",
-      |        "endpointsEnabled":false
-      |      }
-      |    ]
-      |  }
-      |}
-    """.stripMargin)
+    |{
+    |   "scopes":[
+    |      {
+    |         "key":"read:self-assessment",
+    |         "name":"#name#",
+    |         "description":"#desc#"
+    |      },
+    |      {
+    |         "key":"write:self-assessment",
+    |         "name":"#name#",
+    |         "description":"#desc#"
+    |      }
+    |   ],
+    |   "api":{
+    |      "name":"#mtd-api# (MTD)",
+    |      "description":"#desc#",
+    |      "context":"mtd/template",
+    |      "categories":[
+    |         "INCOME_TAX_MTD"
+    |      ],
+    |      "versions":[
+    |         {
+    |            "version":"1.0",
+    |            "status":"ALPHA",
+    |            "endpointsEnabled":false
+    |         }
+    |      ]
+    |   }
+    |}
+    """.stripMargin
+  )
 
   "GET /api/definition" should {
     "return a 200 with the correct response body" in {
-
       val response: WSResponse = await(buildRequest("/api/definition").get())
       response.status shouldBe Status.OK
       Json.parse(response.body) shouldBe apiDefinitionJson
